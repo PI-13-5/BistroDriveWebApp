0de4275f5d270ecdc13cd50676f35925fc1e5e7c
Alex Kruchkov <lexa1805@gmail.com>
2015-10-20 02:30:45 +0300

    Added DataBase + Linq2Sql structure

--- BistroDriveWebApp/App_Start/IdentityConfig.cs
+++ BistroDriveWebApp/App_Start/IdentityConfig.cs
@@ -54,10 +54,10 @@ namespace BistroDriveWebApp
             manager.PasswordValidator = new PasswordValidator
             {
                 RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
+                //RequireNonLetterOrDigit = true,
                 RequireDigit = true,
-                RequireLowercase = true,
-                RequireUppercase = true,
+                //RequireLowercase = true,
+                //RequireUppercase = true,
             };
 
             // Configure user lockout defaults
