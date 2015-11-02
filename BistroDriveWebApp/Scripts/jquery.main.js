5161280ed1661ac9f7eaadf187b08989f38a56d4
Palladis <palladis.ua@gmail.com>
2015-11-02 23:56:44 +0200

    offers without slider

---
+++ BistroDriveWebApp/Scripts/jquery.main.js
@@ -0,0 +1,70 @@
+﻿jQuery(document).ready(function(){
+
+
+/* слайдер цен */
+
+jQuery("#slider").slider({
+	min: 0,
+	max: 1000,
+	values: [0,1000],
+	range: true,
+	stop: function(event, ui) {
+		jQuery("input#minCost").val(jQuery("#slider").slider("values",0));
+		jQuery("input#maxCost").val(jQuery("#slider").slider("values",1));
+		
+    },
+    slide: function(event, ui){
+		jQuery("input#minCost").val(jQuery("#slider").slider("values",0));
+		jQuery("input#maxCost").val(jQuery("#slider").slider("values",1));
+    }
+});
+
+jQuery("input#minCost").change(function(){
+
+	var value1=jQuery("input#minCost").val();
+	var value2=jQuery("input#maxCost").val();
+
+    if(parseInt(value1) > parseInt(value2)){
+		value1 = value2;
+		jQuery("input#minCost").val(value1);
+	}
+	jQuery("#slider").slider("values",0,value1);	
+});
+
+	
+jQuery("input#maxCost").change(function(){
+		
+	var value1=jQuery("input#minCost").val();
+	var value2=jQuery("input#maxCost").val();
+	
+	if (value2 > 1000) { value2 = 1000; jQuery("input#maxCost").val(1000)}
+
+	if(parseInt(value1) > parseInt(value2)){
+		value2 = value1;
+		jQuery("input#maxCost").val(value2);
+	}
+	jQuery("#slider").slider("values",1,value2);
+});
+
+
+
+// фильтрация ввода в поля
+	jQuery('input').keypress(function(event){
+		var key, keyChar;
+		if(!event) var event = window.event;
+		
+		if (event.keyCode) key = event.keyCode;
+		else if(event.which) key = event.which;
+	
+		if(key==null || key==0 || key==8 || key==13 || key==9 || key==46 || key==37 || key==39 ) return true;
+		keyChar=String.fromCharCode(key);
+		
+		if(!/\d/.test(keyChar))	return false;
+	
+	});
+
+
+});
+
+
+
