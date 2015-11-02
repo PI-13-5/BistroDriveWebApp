a94a1af4d6c6e237e66f75db5d02d1cd840f35b2
palladis <palladis@users.noreply.github.com>
2015-11-03 01:55:50 +0200

    Update HomeController.cs

--- BistroDriveWebApp/Controllers/HomeController.cs
+++ BistroDriveWebApp/Controllers/HomeController.cs
@@ -9,7 +9,7 @@ namespace BistroDriveWebApp.Controllers
 {
     public class HomeController : Controller
     {
-        const int PageSize = 20;
+        const int PageSize = 1;
         public ActionResult Index()
         {
             return View();
