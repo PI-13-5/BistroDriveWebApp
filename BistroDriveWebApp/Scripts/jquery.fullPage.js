6e33cf7db4b4c2af59b5b00ab0d0d62f9ded8bba
Palladis <palladis.ua@gmail.com>
2015-12-11 00:32:23 +0200

    about landing and offers pages changed, dishinfo buttons and rating, layout size of top nav

--- BistroDriveWebApp/Scripts/jquery.fullPage.js
+++ BistroDriveWebApp/Scripts/jquery.fullPage.js
@@ -137,7 +137,7 @@
             controlArrowColor: '#fff',
             verticalCentered: true,
             resize: false,
-            sectionsColor : [],
+            sectionsColor : ['#f5f6f7','#fafbfd','#f5f6f7','#f8fcff'],
             paddingTop: 0,
             paddingBottom: 0,
             fixedElements: null,
